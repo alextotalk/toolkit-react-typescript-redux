bbdb4ebbd0ea581abe25eb9ac2eaf166ada24ba2
alextotalk <alexoo@inbox.ru>
2024-01-19 21:46:14 +0200

    first

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//crate description here
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
