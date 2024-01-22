8774cbbf05ac4f476cdb09caf9d345fa3aa64608
alextotalk <78819983+alextotalk@users.noreply.github.com>
2024-01-22 16:33:40 +0000

    feat: Fix formatting, add tasks.json file and add devcontainer.json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+/*  */ import React from "react";
 import MyIframeComponent from "./components/ifram";
 
 function App() {
-    return (
-        <div >
-<MyIframeComponent title="My Title" width={300} height={200} src="https://collabedit.com/turxc"/>
-        </div>
-    );
+  return (
+    <div>
+      <MyIframeComponent
+        title="My Title"
+        width={300}
+        height={200}
+        src="https://collabedit.com/turxc"
+      />
+    </div>
+  );
 }
 
 export default App;
